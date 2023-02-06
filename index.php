c6f0d267af585d078e09148ae3290451fc6034dc
weldoami <weldoami@che>
2023-02-06 09:53:22 +0000

    hello

--- index.php
+++ index.php
@@ -1,12 +1,28 @@
-<?php	
+<?php
 header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
 header("Pragma: no-cache"); // HTTP 1.0.
 header("Expires: 0"); // Proxies.
 ?>
 <H1>Version B</H1>
-<img src="b-abtesting.png">
+<img alt="b-ver" src="b-abtesting.png">
 <div>
-<?php 
+<?php
  echo "My Pod IP is : ".$_SERVER['SERVER_ADDR'] ;
- ?>
+?>
 </div>
+
+<?
+echo("<p>Search results for query: " .
+    $_GET['query'] . ".</p>");
+?>
+<?
+echo("<p>Search results for query: " .
+    htmlspecialchars($_GET['query']) . ".</p>");
+?>
+<?php
+if (isset($_GET['query']))
+{
+  echo '<p>Search results for query: ',
+      htmlspecialchars($_GET['query'], ENT_QUOTES), '.</p>';
+}
+?>
